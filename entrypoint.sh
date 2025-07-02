352abd1ce91fc88a27640245d44357f75f7e3bfe
atzegut <110556998+atzegut@users.noreply.github.com>
2025-07-02 16:14:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 ### ssh-keygen -A
-exec /usr/sbin/sshd -D -e "$@"
+#exec /usr/sbin/sshd -D -e "$@"
+
+ENV HOME="/home/user" TERM="xterm" USER="user" SHELL="/bin/bash" EDITOR="vim" LANG="en_US.UTF-8" LC_ALL="C"
+RUN echo 'root:T3st@123' | chpasswd
+CMD [ "/bin/bash" ]
